5f7091bd46382a6c754f07cc2d10af92f3d1aed9
道前優弥 <>
2018-07-03 16:49:02 +0900

    add file

---
+++ assets/js/function.js
@@ -0,0 +1,19 @@
+var LEIHAUOLI = LEIHAUOLI || {};
+LEIHAUOLI.COMMON = {};
+
+LEIHAUOLI.COMMON.TEST = {
+	init : function(){
+		this.setParameters();
+		this.bindEvent();
+	},
+	setParameters : function(){
+    console.log('ばーか');
+	},
+	bindEvent : function(){
+
+	}
+}
+
+$(function(){
+    LEIHAUOLI.COMMON.TEST.init();
+});
